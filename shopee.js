f6a8008e15167067be9d7b78306e060c89bb85cb
demetrox <33936640+demetrox@users.noreply.github.com>
2023-03-31 16:38:40 +0800

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -1,19 +1,3 @@
-<style type="text/css">
-  .iframe-container {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    border: 0;
-    height: 100%;
-    min-width: 100%;
-    width: 10px;
-    -webkit-overflow-scrolling: touch;
-    overflow-x: auto;
-  }
-</style>
-
 <script>
   setTimeout(function() {
     window.location = "https://shope.ee/8KJ0kguM09";
