82374558fb4b45d1259faf7bdd8ffaaac8931922
demetrox <33936640+demetrox@users.noreply.github.com>
2023-03-31 16:28:06 +0800

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -16,6 +16,6 @@
 
 <script>
   setTimeout(function() {
-    window.location = "https://shope.ee/5fIDlYhp4O";
+    window.location = "https://shope.ee/8KJ0kguM09";
   }, 20000); // 20 seconds delay
 </script>
