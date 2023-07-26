14f7d1f9ada921a2aed91c594569901b78c43aca
demetrox <33936640+demetrox@users.noreply.github.com>
2023-07-26 09:59:37 +0800

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -15,7 +15,28 @@
 </style>
 
 <script>
-  setTimeout(function() {
-    window.location = "https://shope.ee/8KJ0kguM09";
-  }, 20000); // 20 seconds delay
+var lastExecutionTime = localStorage.getItem("lastExecutionTime");
+var currentTime = new Date().getTime();
+
+if (lastExecutionTime && currentTime - lastExecutionTime < 3600000) {
+  // Script has already run within the past hour, exit the script
+  console.log("Script has already run in the past hour.");
+} else {
+  var links = [
+    "https://zass.to/zzn7p",
+    "https://zass.to/ms4mr",
+    "https://zass.to/se07n",
+    "https://zass.to/0xj4s",
+    "https://zass.to/j143n"
+  ];
+
+  var randomIndex = Math.floor(Math.random() * links.length);
+
+  setTimeout(function () {
+    window.location = links[randomIndex];
+  }, 15000);
+
+  // Store the current time in the local storage
+  localStorage.setItem("lastExecutionTime", currentTime.toString());
+}
 </script>
