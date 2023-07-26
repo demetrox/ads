d2daa4fd3695bff30165afb01e71215b254b7fe3
demetrox <33936640+demetrox@users.noreply.github.com>
2023-07-26 10:27:17 +0800

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -1,19 +1,3 @@
-<style type="text/css">
-  .iframe-container {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    border: 0;
-    height: 100%;
-    min-width: 100%;
-    width: 10px;
-    -webkit-overflow-scrolling: touch;
-    overflow-x: auto;
-  }
-</style>
-
 <script>
 var lastExecutionTime = localStorage.getItem("lastExecutionTime");
 var currentTime = new Date().getTime();
