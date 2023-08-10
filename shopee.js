3f9bbd3284ec61df3fc1b778efbeb90580786fdb
demetrox <33936640+demetrox@users.noreply.github.com>
2023-08-10 11:47:37 +0800

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -5,11 +5,11 @@ if (lastExecutionTime && currentTime - lastExecutionTime < 3600000) {
   console.log("Script has already run in the past hour.");
 } else {
   var links = [
-    "https://zass.to/vh2qk",
-    "https://zass.to/npten",
-    "https://zass.to/w1urj",
-    "https://zass.to/yxq68",
-    "https://zass.to/jimzs"
+    "https://c.lazada.com.my/t/c.cGkaD1",
+    "https://c.lazada.com.my/t/c.cGkaD2",
+    "https://c.lazada.com.my/t/c.cGkaDd",
+    "https://c.lazada.com.my/t/c.cGkaD3",
+    "https://c.lazada.com.my/t/c.cGkaDg"
   ];
 
   var randomIndex = Math.floor(Math.random() * links.length);
